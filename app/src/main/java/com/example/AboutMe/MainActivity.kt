e7bde86f6562f4cd95b16d70c4ac815c50c5f2ee
Eric Womer <ewomer@gmail.com>
2022-04-30 16:31:35 -0400

    initial commit

---
+++ app/src/main/java/com/example/AboutMe/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.AboutMe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.content.Context
+import android.widget.Toast
+
+// Project specific imports
+import com.example.AboutMe.R
+
+public class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
